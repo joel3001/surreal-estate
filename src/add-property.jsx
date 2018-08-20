8cf35be62531d65e7726dc5dba61905015df60aa
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-08-20 22:33:16 +0100

    Step 4 started - form and event handlers

--- src/add-property.jsx
+++ src/add-property.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../styles/add-property.scss';
 
-const AddProperty = () => (
-  <div>Add Property Page</div>
-);
+class AddProperty extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fields: {
+        title: '',
+        type: '',
+        city: '',
+      },
+    };
+  }
+
+  handleAddProperty = event => {
+    event.preventDefault();
+
+    console.log(this.state.fields);
+  };
+
+  handleFieldChange = event => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+
+  render() {
+    return (
+      <div className="add-property">
+        Add Property Page
+        <form onSubmit={this.handleAddProperty}>
+          <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} placeholder="Property Title" />
+          <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End-of-terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddProperty;
