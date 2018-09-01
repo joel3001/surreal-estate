5a758a675b61d7e6cc123535b974f35aadc3654b
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-09-01 20:15:23 +0100

    Properties class component created - multple propertiess from API now listed

--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -1,13 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import PropertyCard from './property-card';
+import axios from 'axios';
+import '../styles/properties.scss';
 
-const Properties = () => (
-  <div className="Properties">
-    <Fragment>
-      Properties Page
-      <PropertyCard />
-    </Fragment>
-  </div>
-);
+class Properties extends Component {
+  constructor() {
+    super();
+    this.state = {
+      properties: [],
+      isError: false,
+      alertMessage: '',
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/v1/PropertyListing')
+      .then(({ data: properties }) => this.setState({ properties }))
+      .catch((err) => {
+        this.setState({
+          isError: true,
+          alertMessage: 'Server error, properties currently unavailable, please try again later',
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="Properties">
+        {this.state.properties.map(property => (
+          <div key={property._id} className="col">
+            <PropertyCard {...property} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Properties;
--- src/components/property-card.jsx
+++ src/components/property-card.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const PropertyCard = (props) => (
   <div className="property-card">
